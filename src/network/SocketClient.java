7d35bd58d419f090f16d69ff82949496ec42fab1
eduardo <11eduardoalmeida@gmail.com>
2026-04-24 00:08:42 -0300

    feat(network): add SocketClient

---
+++ src/network/SocketClient.java
@@ -0,0 +1,31 @@
+package network;
+
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class SocketClient {
+
+    // um metodo fire and forget basicamente
+    public static void enviarMensagem(String ipDestino, int portaDestino, String mensagem) {
+        try {
+            // 1. tenta abrir um tubo TCP direto com o IP e a porta do vizinho
+            Socket socket = new Socket(ipDestino, portaDestino);
+            
+            // 2. cria o canal de saida de texto (o 'true' força o envio imediato, sem ficar preso no buffer)
+            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+            
+            // 3. grita a mensagem pelo tubo
+            out.println(mensagem);
+            
+            // 4. fecha a conexao liberando recursos de rede 
+            socket.close();
+            
+            System.out.println("[CLIENTE] Mensagem enviada com sucesso para " + ipDestino + ":" + portaDestino);
+            
+        } catch (Exception e) {
+            // Se o vizinho estiver com o PC desligado ou o Firewall bloqueando, cai aqui.
+            // O try/catch blinda o nosso backend para que nosso jogo n crashe!
+            System.out.println("[CLIENTE] Falha ao conectar com " + ipDestino + ":" + portaDestino + ". O nó parece estar offline.");
+        }
+    }
+}
